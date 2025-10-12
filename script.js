db36434077763523382e6a6edf85e338545035f3
reo1eo <gibsongonzales09@gmail.com>
2025-10-12 15:59:15 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+document.querySelectorAll('.navbar a').forEach(link => {
+  link.addEventListener('click', function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
