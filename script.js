bf962912f7de0db1a997d07153a783745cbfe4d9
reo1eo <gibsongonzales09@gmail.com>
2025-10-14 00:38:18 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
-document.querySelectorAll('.navbar a').forEach(link => {
-  link.addEventListener('click', function (e) {
-    e.preventDefault();
-    document.querySelector(this.getAttribute('href')).scrollIntoView({
-      behavior: 'smooth'
+navLinks.forEach(link => {
+    link.addEventListener('click', function (e) {
+        const href = this.getAttribute('href');
+        if (href.startsWith('#')) {
+            e.preventDefault();
+            const targetElement = document.querySelector(href);
+            
+            if (targetElement) {
+                targetElement.scrollIntoView({
+                    behavior: 'smooth'
+                });
+            }
+            navLinks.forEach(l => l.classList.remove('active'));
+            this.classList.add('active');
+        }
     });
-  });
 });
